2b655657835d5d84638764f2f2d4736f08a99aa3
Ollie <oliver.siket@gmail.com>
2019-11-13 13:39:14 +0000

    Initial setup with Laravel

---
+++ resources/views/bio.blade.php
@@ -0,0 +1,33 @@
+<html class="no-js" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="apple-touch-icon" href="favicon.png">
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<!-- Main content -->
+
+<body> 
+
+        <!-- Header Content -->
+    <header>
+
+    </header>
+
+        <!-- Main Content -->
+    <main>
+        <h2>Test</h2>
+    </main>
+
+        <!-- Footer Content -->
+    <footer>
+
+    </footer>
+</body>
+</html>
